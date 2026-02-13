540ddfe426bd2e409f10dc1bb646b777133bca78
lavanya986 <lavanyam986@gmail.com>
2026-02-13 10:00:43 +0530

    Refactor Hello class to Main and fix syntax errors

--- Hello.java
+++ Hello.java
@@ -1,5 +1,5 @@
-public class Hello{
-  public static void main(String {} arg) {
-    System.out.println("Hello world"):
-      }
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
 }
